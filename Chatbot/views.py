e26a1ec0707dc7ddb3dc8dd35ca1d3fd2424093e
zurijang <wkdwn05@gmail.com>
2020-11-24 11:33:51 +0900

    압축된 이미지 등록가능

--- Chatbot/views.py
+++ Chatbot/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 from telegram_webhook import bot
 from .tasks import is_json_key_present
 from .models import DATA_IMAGE, DATA_WORKER, QUESTION, ANSWER
-from .text import start, regist, pause, usage
+from .text import start, regist, pause, usage, image
 import telegram
 import json
 
@@ -27,12 +27,19 @@ def webhook(request):
 
     fileName_list = [] # 파일 저장명
 
+    # 이미지 파일인지 확인
     if ((is_json_key_present(telegram_update['message'], 'document')) and (telegram_update['message']['document']['mime_type'] == "image/jpeg" \
-        or telegram_update['message']['document']['mime_type'] == "image/png")): # 압축되지않은 Image 형식, 'document' key가 있는지 확인 (True, False)
+        or telegram_update['message']['document']['mime_type'] == "image/png")) or is_json_key_present(telegram_update['message'], 'photo'):
         
-        print('document/image')
-        fileID = telegram_update['message']['document']['file_id'] # document의 file_id 추출, 이미지 저장에 활용
-        fileUniqueID = telegram_update['message']['document']['file_unique_id'] # document의 file_unique_id 추출, 파일명에 활용
+        try: # 압축되지 않은 이미지
+            print('document/image')
+            fileID = telegram_update['message']['document']['file_id'] # document의 file_id 추출, 이미지 저장에 활용
+            fileUniqueID = telegram_update['message']['document']['file_unique_id'] # document의 file_unique_id 추출, 파일명에 활용
+
+        except KeyError: # 압축된 이미지
+            print('photo')
+            fileID = telegram_update['message']['photo'][1]['file_id'] # photo의 fild_id 추출, 이미지 저장에 활용
+            fileUniqueID = telegram_update['message']['photo'][1]['file_unique_id'] # photo의 file_unique_id 추출, 파일명에 활용
 
         # 파일명 전처리
         fileName_list.append(base_path)
@@ -65,13 +72,14 @@ def webhook(request):
                 # 등록된 질문이 7개이고, 7번째 질문에 답변이 되어있어야 이미지 등록 가능, 혹은 /중지 입력시 (STATUS = 9)
                 if ((QUESTION.objects.filter(IMAGE_ID=load_image[0]).count()>=7) and (ANSWER.objects.filter(QUESTION_ID=load_question[0]).count()==1)) \
                     or (load_image[0].STATUS=='9'):
-                    # 질문 답 7개 이상 등록 후 이미지 등록할 경우 
+
+                    # 질문 답 7개 이상 등록 후 이미지 등록할 경우 등록진행중이던 이미지 STATUS 1로 변경
                     if QUESTION.objects.filter(IMAGE_ID=load_image[0]).count()>=7:
                         print('CHANGE STATUS to 1')
                         DATA_IMAGE.objects.filter(IMAGE_ID=load_image[0].IMAGE_ID).update(STATUS=1)
                         QUESTION.objects.filter(IMAGE_ID=load_image[0]).update(STATUS=1)
 
-                    # 이미지 경로 DB 저장
+                    # 이미지 DB 저장
                     DATA_IMAGE.objects.create(
                         WORKER_ID = create_worker,
                         IMAGE = fileName)
@@ -81,7 +89,7 @@ def webhook(request):
                     # 이미지 저장
                     file = bot.getFile(fileID)
                     file.download(fileName)
-                    text = '데이터 등록을 시작합니다. 1번째 질문을 입력해주세요.'
+                    text = '데이터 등록을 시작합니다. 1번째 질문을 입력하세요.'
                     print(fileName)
                     print('이미지 저장 완료')
 
@@ -92,10 +100,12 @@ def webhook(request):
                         text = '7개 질문과 그에대한 답을 모두 입력해주세요.\n' + str(load_question[0].QUESTION_CNT + 1) + '번째 질문을 입력하세요.'
                     else:
                         text = '7개 질문과 그에대한 답을 모두 입력해주세요.\n' + str(load_question[0].QUESTION_CNT) + '번째 답을 입력하세요.'
+
             except IndexError:
-                print('Aleardy regist Image : 이미지 연속 등록')
-                text = "진행중인 이미지에 대한 질문 답을 모두 입력해주세요."
+                print('Already regist Image : 이미지 연속 등록')
+                text = "진행중인 이미지에 대한 1번째 질문을 입력해주세요."
         
+        # 처음 사용하는 유저가 이미지 등록할 때
         except IndexError:
             # try:
             print('Regist Image : 사용자 첫 이미지 등록')
@@ -115,10 +125,9 @@ def webhook(request):
         
         bot.send_message(chat_id=chatID, text=text)
 
-        # 저장 완료되었을 때 질문 요구
         
     else: # Text 형식, 질문, 답
-        if is_json_key_present(telegram_update['message'], 'text'): # TEXT만 받도록, IMAGE 외의 documnet (파일) 거름
+        if is_json_key_present(telegram_update['message'], 'text'): # TEXT만 받도록
             print('TEXT')
             try: # 처음 등록하는 유저가 이미지 등록되지 않은 상태로 질문, 답 할때 예외처리
 
@@ -135,7 +144,16 @@ def webhook(request):
                 if received_text=='/사용방법':
                     print('사용방법')
                     bot.send_message(chat_id=chatID, text=usage)
-                    bot.send_message(chat_id=chatID, text='이어서 등록해주세요.')
+                    bot.send_message(chat_id=chatID, text=image)
+                    try:
+                        if ANSWER.objects.filter(QUESTION_ID=load_question[0]).count() == 0:
+                            text = str(load_question[0].QUESTION_CNT) + ' 번째 답변을 입력해주세요.'
+                        else:
+                            text = str(load_question[0].QUESTION_CNT + 1) + ' 번째 질문을 입력해주세요.'
+
+                    except IndexError:
+                        text = '1번째 질문을 입력해주세요.'
+                    bot.send_message(chat_id=chatID, text=text)
                     return HttpResponse("사용방법")
 
                 print(type(recent_image.STATUS))
@@ -150,41 +168,40 @@ def webhook(request):
                     print('CHANGE STATUS to 9')
                     DATA_IMAGE.objects.filter(IMAGE_ID=recent_image.IMAGE_ID).update(STATUS=9)
                     QUESTION.objects.filter(IMAGE_ID=recent_image).update(STATUS=9)
-                    text = '중지되었습니다. 새로운 이미지를 등록해주세요.'
-                    bot.send_message(chat_id=chatID, text=text)
+                    bot.send_message(chat_id=chatID, text='중지되었습니다. 새로운 이미지를 등록해주세요.')
                     return HttpResponse("중지됨")
                 
                 if received_text[-1] == '?': # 질문 입력받음
                     print('question')
-                    try:
-                        if load_question.count()==0: # 이미지에 대한 질문 유무확인
-                            print('첫 번째 질문 등록')
-                            question_cnt = 1 # 질문이 없을 경우 첫 번째 질문
-                            # 이미지에 대한 질문 등록 (처음)
+                    # try:  ## 없어도 되는 부분??
+                    if load_question.count()==0: # 이미지에 대한 질문 유무확인
+                        print('첫 번째 질문 등록')
+                        question_cnt = 1 # 질문이 없을 경우 첫 번째 질문
+                        # 이미지에 대한 질문 등록 (처음)
+                        QUESTION.objects.create(
+                            IMAGE_ID=recent_image, # 사용자가 가장 최근에 등록한 이미지
+                            QUESTION=received_text, # 입력받은 질문
+                            QUESTION_CNT=question_cnt # 이미지에 대한 질문 수
+                        )
+                        text = str(question_cnt) + '번째 질문에 대한 답변을 입력하세요.' 
+                    else:
+                        question_cnt = load_question[0].QUESTION_CNT + 1 # 이미지에 대한 질문이 있을 경우, 기존에 등록된 QUESTION_CNT에서 + 1
+                        if ANSWER.objects.filter(QUESTION_ID=load_question[0].QUESTION_ID).count() == 0:
+                            print('질문에 대한 답변 없음')
+                            text = str(question_cnt-1) + '번째 질문에 대한 답변을 먼저 입력해주세요.' 
+                        else:
+                            print('질문 등록')
+                            # 이미지에 대한 질문 등록 (두번째 이후)
                             QUESTION.objects.create(
                                 IMAGE_ID=recent_image, # 사용자가 가장 최근에 등록한 이미지
                                 QUESTION=received_text, # 입력받은 질문
                                 QUESTION_CNT=question_cnt # 이미지에 대한 질문 수
                             )
-                            text = str(question_cnt) + '번째 질문에 대한 답변을 입력해주세요.' 
-                        else:
-                            question_cnt = load_question[0].QUESTION_CNT + 1 # 이미지에 대한 질문이 있을 경우, 기존에 등록된 QUESTION_CNT에서 + 1
-                            if ANSWER.objects.filter(QUESTION_ID=load_question[0].QUESTION_ID).count() == 0:
-                                print('질문에 대한 답변 없음')
-                                text = str(question_cnt-1) + '번째 질문에 대한 답변을 먼저 입력해주세요.' 
-                            else:
-                                print('질문 등록')
-                                # 이미지에 대한 질문 등록 (두번째 이후)
-                                QUESTION.objects.create(
-                                    IMAGE_ID=recent_image, # 사용자가 가장 최근에 등록한 이미지
-                                    QUESTION=received_text, # 입력받은 질문
-                                    QUESTION_CNT=question_cnt # 이미지에 대한 질문 수
-                                )
-                                text = str(question_cnt) + '번째 질문에 대한 답변을 입력해주세요.'
+                            text = str(question_cnt) + '번째 질문에 대한 답변을 입력하세요.'
 
-                    except IndexError: # 사용자가 등록한 이미지가 없을 경우
-                        print('진행중인 사진 없음')
-                        text = '등록된 사진이 없습니다.'
+                    # except IndexError: # 사용자가 등록한 이미지가 없을 경우, 없어도 되는 부분?
+                    #     print('진행중인 사진 없음')
+                    #     text = '등록된 사진이 없습니다. 사진을 등록해주세요.'
 
                     bot.send_message(chat_id=chatID, text=text)  
 
@@ -208,25 +225,27 @@ def webhook(request):
 
                         else:
                             print("답변이 이미 존재")
-                            text = '새로운 질문을 입력하세요.'
+                            text = str(load_question[0].QUESTION_CNT+1) + '번째 질문을 먼저 입력해주세요.'
                         
                     except IndexError:
                         print("등록된 질문 아예 없음")
-                        text = '등록된 질문이 없습니다.'
+                        text = '1번째 질문을 먼저 입력해주세요.'
 
                     bot.send_message(chat_id=chatID, text=text)  
 
-            except IndexError: # 등록되지 않은 사용자가 텍스트 입력한 경우
+            except IndexError: # 사용자가 등록한 이미지가 없는 경우
                 # text = start
                 bot.send_message(chat_id=chatID, text=start)
                 bot.send_message(chat_id=chatID, text=regist)
+                bot.send_message(chat_id=chatID, text=usage)
                 bot.send_message(chat_id=chatID, text=pause)
+                bot.send_message(chat_id=chatID, text=image + '\n▶ 이미지를 보내면 등록이 시작됩니다.')
+                
         else: # 텍스트, 이미지 외의 파일 전송
-            print('압축된 파일 전송')
+            print('텍스트, 이미지 외의 파일 전송')
             text = '''이미지를 전송해주세요.
-            android)첨부파일→파일→갤러리(압축 없이 사진 보내기)
-            ios)첨부파일→파일→사진및동영상
-            web)'send file'통해서 이미지 전송'''
+{0}'''.format(image)
+            
             bot.send_message(chat_id=chatID, text=text)
 
     return HttpResponse("webhook")
